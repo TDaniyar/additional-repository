22e8c04d89f794a982bcb4465f8c406340e457e4
Daniyar Tule <daniyartule@gmail.com>
2021-12-02 16:00:30 +0300

    task 06

--- script.js
+++ script.js
@@ -1,101 +1,72 @@
 'use strict';
 
-let title
-let screens
-let screenPrice
-let adaptive
-let screensNew
+/* Для этого задания создайте отдельный репозиторий.
+Используйте функции alert, confirm, prompt для общения с пользователем.
+Написать игровой бот.
+"Загадывание случайного числа от 1 до 100"
+Что должна делать программа:
+— спрашивает пользователя: "Угадай число от 1 до 100".
+— если пользовательское число больше, то бот выводит "Загаданное число меньше" и предлагает ввести новый вариант;
+— если пользовательское число меньше, то бот выводит "Загаданное число больше" и предлагает ввести новый вариант;
+— если пользователь ввел не число, то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
+— если пользователь нажимает "Отмена", то игра заканчивается и выводится сообщение "Игра окончена".
+— если пользовательское число равно загаданному, то игра заканчивается и выводит сообщение "Поздравляю, Вы угадали!!!".
+Программа должны быть выполнена с помощью рекурсии, без единого цикла.
+Загаданное число должно храниться «в замыкании»
+*/
+let greeting;
+let gameExplanation;
+let gameStartNumber;
+let undefinedNumber;
+let gameNumberisNumber;
 
-let allServicePrices
-let fullPrice
-let rollback
-let servicePercentPrice
-let service1
-let service2
 
-const isNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-}
-
-const asking = function() {
-    title = prompt('Как называется ваш проект?', "Калькулятор верстки")
-    screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные")
-    do {
-    screenPrice = +prompt('Сколько будет стоить данная работа?')
-    }
-    while (!isNumber(screenPrice) || screenPrice == 0)
-
-    adaptive = confirm('Нужен ли адаптив на сайте?')
-    screensNew = screens.toLowerCase()
-}
-
-const getAllServicePrices = function() {
-    let sum = 0
-    for (let i=0; i<2; i++){
-        if (i===0){
-            service1 = prompt('Какой дополнительный тип услуги нужен?', 'Простые матрицы')
-        } else if(i===1){
-            service2 = prompt('Какой дополнительный тип услуги нужен?', 'Сложные матрицы');
-        }
-        sum += +prompt("Сколько это будет стоить?")
-        while (!isNumber(sum)){
-            sum = +prompt("Сколько это будет стоить?")
-        }
-    }
-    return sum
-}
-
-const showTypeOf = function(variable){
-    console.log(variable, typeof variable);
-}
-
-function getFullPrice () {
-    return screenPrice + allServicePrices
-}
-
-function getRollback () {
-    return fullPrice*0.1
-}
-
-const getServicePercentPrices = function(){
-    return fullPrice - rollback
-}
-
-function getRollbackMessage(price) {
-    if (price > 30000) {
-        return "Даем скидку в 10%";
-    } else if (price >= 15000 && fullPrice < 30000) {
-        return "Даем скидку в 5%";
-    } else if (price <= 15000 && fullPrice > 0) {
-        return "Скидка не предусмотрена";
+const gameStart = function() {
+    greeting = alert("Загадывание случайного числа от 1 до 100");
+    gameExplanation = confirm("Ты хочешь угадай число от 1 до 100 ?");
+    if (gameExplanation === true) {
+        gameStartNumber = +prompt("Загадай число от 1 до 100");
+        const isNumber = function(gameStartNumber) {
+            return !isNaN(parseFloat(gameStartNumber)) && isFinite(gameStartNumber);
+        };
+            gameNumberisNumber = isNumber(gameStartNumber);
+        if (gameNumberisNumber === true) {
+            const gamerunning = function(num1, num2){
+                if (num1 > num2){
+                    return gamerunning(+prompt("Загаданное число больше, Введи новое число!"),num2);
+                } else if (num1 < num2) {
+                    return gamerunning(+prompt("Загаданное число меньше, Введи новое число!"),num2);
+                } else if (num1 === num2) {
+                    return alert("Поздравляю, Вы угадали!!!");
+                }
+                gamerunning(gameNumberisNumber,undefinedNumber);
+            };
+        }else if(gameNumberisNumber === false) {
+            return isNumber(+prompt("Введи число!"));
+        }else if(gameNumberisNumber===0){
+            return alert("Игра окончена");
+            }
     } else {
-        return "Что то пошло не так";
+        alert("До свидания!");
     }
-}
+};
+// const gamerunning = function(num1, num2) {
+//     if (num1 > num2){
+//         return gamerunning(+prompt("Загаданное число больше, Введи новое число!"),num2);
+//     } else if (num1 < num2){
+//         return gamerunning(+prompt("Загаданное число меньше, Введи новое число!"),num2);
+//     } else if (num1 === num2){
+//         return alert("Поздравляю, Вы угадали!!!");
+//     } else if  (num1 === 0) {
+//         return alert("Игра окончена");
+//     } else if (num1.isNaN()){
+//         return +prompt("Введи число!");
+//     }
+// };
 
-const getTitle = function (str) {
-    let str1 = str.trim()
-    let string = str1.toLowerCase()
-    let newTitle = string[0].toUpperCase() + string.slice(1);
-    return newTitle
-}
-asking()
-allServicePrices = getAllServicePrices()
-fullPrice = getFullPrice ();
-rollback = getRollback(fullPrice);
-servicePercentPrice = getServicePercentPrices();
+undefinedNumber = Math.floor(Math.random() * 10);
+gameStart();
+//gamerunning(gameStartNumber, undefinedNumber);
 
-showTypeOf(title)
-showTypeOf(fullPrice)
-showTypeOf(adaptive)
-
-console.log(allServicePrices)
-console.log(getTitle(title));
-console.log(screens.length);
-console.log("Стоимость верстки экранов " + (screenPrice) + " рублей");
-console.log("Стоимость разработки сайта " + (fullPrice) + " рублей");
-console.log(screensNew.split(" "));
-console.log(fullPrice * (rollback / 100));
-console.log(getRollbackMessage(fullPrice));
-console.log(getServicePercentPrices());
-
+console.log(undefinedNumber);
+console.log(gameStartNumber);
