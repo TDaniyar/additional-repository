69e5e499453dea43c08bde5f55468dee8ac9c822
Daniyar Tule <daniyartule@gmail.com>
2021-12-02 21:58:36 +0300

    задание 06. Игра угадай число

--- script.js
+++ script.js
@@ -1,72 +1,46 @@
 'use strict';
 
-/* Для этого задания создайте отдельный репозиторий.
-Используйте функции alert, confirm, prompt для общения с пользователем.
-Написать игровой бот.
-"Загадывание случайного числа от 1 до 100"
-Что должна делать программа:
-— спрашивает пользователя: "Угадай число от 1 до 100".
-— если пользовательское число больше, то бот выводит "Загаданное число меньше" и предлагает ввести новый вариант;
-— если пользовательское число меньше, то бот выводит "Загаданное число больше" и предлагает ввести новый вариант;
-— если пользователь ввел не число, то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
-— если пользователь нажимает "Отмена", то игра заканчивается и выводится сообщение "Игра окончена".
-— если пользовательское число равно загаданному, то игра заканчивается и выводит сообщение "Поздравляю, Вы угадали!!!".
-Программа должны быть выполнена с помощью рекурсии, без единого цикла.
-Загаданное число должно храниться «в замыкании»
-*/
 let greeting;
 let gameExplanation;
 let gameStartNumber;
 let undefinedNumber;
 let gameNumberisNumber;
 
-
-const gameStart = function() {
+const gameStart = function () {
     greeting = alert("Загадывание случайного числа от 1 до 100");
-    gameExplanation = confirm("Ты хочешь угадай число от 1 до 100 ?");
+    gameExplanation = confirm("Ты хочешь угадать число от 1 до 100 ?");
     if (gameExplanation === true) {
-        gameStartNumber = +prompt("Загадай число от 1 до 100");
-        const isNumber = function(gameStartNumber) {
-            return !isNaN(parseFloat(gameStartNumber)) && isFinite(gameStartNumber);
-        };
-            gameNumberisNumber = isNumber(gameStartNumber);
-        if (gameNumberisNumber === true) {
-            const gamerunning = function(num1, num2){
-                if (num1 > num2){
-                    return gamerunning(+prompt("Загаданное число больше, Введи новое число!"),num2);
-                } else if (num1 < num2) {
-                    return gamerunning(+prompt("Загаданное число меньше, Введи новое число!"),num2);
-                } else if (num1 === num2) {
-                    return alert("Поздравляю, Вы угадали!!!");
-                }
-                gamerunning(gameNumberisNumber,undefinedNumber);
-            };
-        }else if(gameNumberisNumber === false) {
-            return isNumber(+prompt("Введи число!"));
-        }else if(gameNumberisNumber===0){
-            return alert("Игра окончена");
+        const getGameStartNumber = function () {
+            gameStartNumber = +prompt("Введи число от 1 до 100");
+            const isNumber = function (num) {
+                return !isNaN(parseFloat(num)) && isFinite(num);
             }
-    } else {
-        alert("До свидания!");
+            gameNumberisNumber = isNumber(gameStartNumber)
+            if (gameNumberisNumber === true) {
+                const gamerunning = function (num1, num2) {
+                    if (isNaN(num1) === true) {
+                        return getGameStartNumber(alert("Введи число!"))
+                    } else if (num1 === 0) {
+                        return alert("Игра окончена")
+                    } else if (num1 > num2) {
+                        return gamerunning(+prompt("Загаданное число больше, Введи новое число!"), num2);
+                    } else if (num1 < num2) {
+                        return gamerunning(+prompt("Загаданное число меньше, Введи новое число!"), num2);
+                    } else if (num1 === num2) {
+                        return alert("Поздравляю, Вы угадали!!!");
+                    }
+                }
+                gamerunning(gameStartNumber, undefinedNumber)
+            } else if (gameNumberisNumber === false) {
+                return getGameStartNumber(alert("Введи число!"));
+            }
+            console.log(isNumber(gameStartNumber))
+        }
+        getGameStartNumber()
     }
-};
-// const gamerunning = function(num1, num2) {
-//     if (num1 > num2){
-//         return gamerunning(+prompt("Загаданное число больше, Введи новое число!"),num2);
-//     } else if (num1 < num2){
-//         return gamerunning(+prompt("Загаданное число меньше, Введи новое число!"),num2);
-//     } else if (num1 === num2){
-//         return alert("Поздравляю, Вы угадали!!!");
-//     } else if  (num1 === 0) {
-//         return alert("Игра окончена");
-//     } else if (num1.isNaN()){
-//         return +prompt("Введи число!");
-//     }
-// };
 
-undefinedNumber = Math.floor(Math.random() * 10);
+}
+
+undefinedNumber = Math.floor(Math.random() * 99 + 1);
 gameStart();
-//gamerunning(gameStartNumber, undefinedNumber);
 
-console.log(undefinedNumber);
-console.log(gameStartNumber);
